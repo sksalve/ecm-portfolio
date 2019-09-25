12261254ef48097686b992d0fd83f21eceb60d5e
sksalve <sachinsalve90@outlook.com>
2019-09-26 02:12:16 +0530

    components refactored

---
+++ src/components/menu-items/menu-item.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './menu-items.styles.css'
+
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="content">
+      <div className="title">
+        <div className="subtitle"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default MenuItem;
