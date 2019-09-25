ea8986e0d0bffacdd9b0873db1cb09ba52efda62
sksalve <sachinsalve90@outlook.com>
2019-09-26 01:34:37 +0530

    React project files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <h1> 
+        <HomePage />
+      </h1>
+     
+    </div>
+  );
+}
+
+export default App;
